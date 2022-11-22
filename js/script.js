e8708720b42b81b5534e0a749458cf5af638ff76
millooo94 <camillomusmeciunict@gmail.com>
2022-11-22 15:38:02 +0100

    add All

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const app = new Vue({
     el: '.container',
     data: {
+        ArrEmail: []
 
     },
     methods: {
 
+
+
     },
+    created(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(axiosResponse => this.ArrEmail.push(axiosResponse.data.response));
+        }
+    }
 })
