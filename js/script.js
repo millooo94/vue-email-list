95105071dce94265851862a34b72f6be80288ac4
millooo94 <camillomusmeciunict@gmail.com>
2022-11-22 15:55:04 +0100

    refactor all

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ const app = new Vue({
 
     },
     methods: {
+        generateAgain(){
+            this.ArrEmail.splice(0, this.ArrEmail.length)
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(axiosResponse => this.ArrEmail.push(axiosResponse.data.response));
+            }
+        }
 
 
 
